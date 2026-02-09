2ed84e512d496bafaaeec4074cc039ac8ab8e0a3
Ammon-Philipp <84660346+Ammon-Philipp@users.noreply.github.com>
2026-02-09 14:49:37 +0100

    feat: Add Web exchange view and viewmodels.

---
+++ CryptoExchangeImporter.Web/ViewModels/ExchangesViewModels.cs
@@ -0,0 +1,30 @@
+﻿namespace CryptoExchangeImporter.Web.ViewModels;
+
+public sealed class ExchangeListItemViewModel
+{
+    public string Id { get; set; } = "";
+    public DateTimeOffset CreatedAt { get; set; }
+    public decimal Crypto { get; set; }
+    public decimal Euro { get; set; }
+}
+
+public sealed class ExchangeDetailsViewModel
+{
+    public string Id { get; set; } = "";
+    public DateTimeOffset CreatedAt { get; set; }
+    public decimal Crypto { get; set; }
+    public decimal Euro { get; set; }
+
+    public List<OrderRowViewModel> Bids { get; set; } = new();
+    public List<OrderRowViewModel> Asks { get; set; } = new();
+}
+
+public sealed class OrderRowViewModel
+{
+    public string OrderId { get; set; } = "";
+    public DateTimeOffset Time { get; set; }
+    public string Type { get; set; } = "";
+    public string Kind { get; set; } = "";
+    public decimal Amount { get; set; }
+    public decimal Price { get; set; }
+}
