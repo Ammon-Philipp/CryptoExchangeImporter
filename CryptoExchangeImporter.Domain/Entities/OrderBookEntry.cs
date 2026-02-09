b4d44227abbb559bdea73d0e64f37e487a6b0923
Ammon-Philipp <84660346+Ammon-Philipp@users.noreply.github.com>
2026-02-09 14:44:00 +0100

    docs: Add minimal inline ADR on entity OrderBookEntry.

--- CryptoExchangeImporter.Domain/Entities/OrderBookEntry.cs
+++ CryptoExchangeImporter.Domain/Entities/OrderBookEntry.cs
@@ -3,8 +3,8 @@
 public sealed class OrderBookEntry
 {
     public int Id { get; set; }
-    // TODO: Add ADR?
-    public bool IsBid { get; set; } // true = Bid, false = Ask => Avoid junction table.
+    // TODO: Add ADR? => Avoid separate tables and Unions for EF Core queries. Use enum instead for more than two collections.
+    public bool IsBid { get; set; } // true = Bid, false = Ask.
 
     // Foreign Key
     public int OrderBookId { get; set; }
