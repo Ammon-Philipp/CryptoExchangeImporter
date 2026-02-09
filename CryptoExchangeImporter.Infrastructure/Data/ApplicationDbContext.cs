40eab92d73999b224492e09d7833e8bd4fff7e98
Ammon-Philipp <84660346+Ammon-Philipp@users.noreply.github.com>
2026-02-09 14:41:19 +0100

    feat: Add AppDbContext and DbSets.

---
+++ CryptoExchangeImporter.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+
+using CryptoExchangeImporter.Domain.Entities;
+
+namespace CryptoExchangeImporter.Infrastructure.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Exchange> Exchanges => Set<Exchange>();
+    public DbSet<AvailableFunds> AvailableFunds => Set<AvailableFunds>();
+    public DbSet<OrderBook> OrderBooks => Set<OrderBook>();
+    public DbSet<OrderBookEntry> OrderBookEntries => Set<OrderBookEntry>();
+    public DbSet<Order> Orders => Set<Order>();
+}
