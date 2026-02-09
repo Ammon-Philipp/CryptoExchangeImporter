57b0c967c0a54926483b426aaf2da0570da7d04f
Ammon-Philipp <84660346+Ammon-Philipp@users.noreply.github.com>
2026-02-09 14:43:50 +0100

    docs: Add PR todo for AvailableFunds entity.

--- CryptoExchangeImporter.Domain/Entities/AvailableFunds.cs
+++ CryptoExchangeImporter.Domain/Entities/AvailableFunds.cs
@@ -1,6 +1,6 @@
 ﻿namespace CryptoExchangeImporter.Domain.Entities;
 
-public sealed class AvailableFunds
+public sealed class AvailableFunds  // TODO: Check in PR: Record due to Value Object. => Here class due to EF Core pragmatism (easier mapping / update / tracking).
 {
     public int Id { get; set; }
     public decimal Crypto { get; set; }
