c4648ecae61634a3878531092038b6505187d788
Ammon-Philipp <84660346+Ammon-Philipp@users.noreply.github.com>
2026-02-09 14:43:34 +0100

    refactor: Cleanup using directives.

--- CryptoExchangeImporter.Domain/Entities/OrderBookEntry.cs
+++ CryptoExchangeImporter.Domain/Entities/OrderBookEntry.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace CryptoExchangeImporter.Domain.Entities;
+﻿namespace CryptoExchangeImporter.Domain.Entities;
 
 public class OrderBookEntry
 {
